0fdad254c3b9c5a93b3a1e4ceb05925f97aac2a8
vredchasing <115098964+vredchasing@users.noreply.github.com>
2024-06-27 14:07:48 -0400

    Update HeaderMain.jsx

--- src/HeaderMain.jsx
+++ src/HeaderMain.jsx
@@ -7,11 +7,6 @@ function HeaderMain(){
         <header className='header'>
             <nav className="navbar">
                 <div className='logo-container'><h1 className="logo">VISIO</h1></div>
-                <div className="middleheader">
-                    <h2 className="middle-header-links">HOME</h2>
-                    <h2 className="middle-header-links">PROJECTS</h2>
-                    <h2 className="middle-header-links">CONTACT US</h2>
-                </div>
             </nav>
         </header>
     )
